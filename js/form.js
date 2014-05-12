c2b6a8edc583360965278f8214dd40692d3fd159
omniaura <omniaura5@gmail.com>
2014-05-11 20:58:31 -0700

    generate radio buttons with js

---
+++ js/form.js
@@ -0,0 +1,402 @@
+$(document).ready(function(){
+    var dimensions_midpoint = Math.ceil(barn_dimensions.length / 2);
+    for (var i = 0; i < dimensions_midpoint; i++) {
+        appendBarnRadioButton(barn_dimensions[i], '#dimensions .col.c1');
+    }
+    console.log(dimensions_midpoint);
+    for (var i = dimensions_midpoint; i < barn_dimensions.length; i++) {
+        appendBarnRadioButton(barn_dimensions[i], '#dimensions .col.c2');
+    }
+});
+
+function appendBarnRadioButton(dimension, elem) {
+    var container = $('<div/>', {
+        'class': 'radio-option',
+        'data-width-feet': dimension.width.feet,
+        'data-width-inches': dimension.width.inches,
+        'data-length-feet': dimension.length.feet,
+        'data-length-inches': dimension.length.inches,
+        'data-height-feet': dimension.height.feet,
+        'data-height-inches': dimension.height.inches,
+    }).appendTo(elem);
+
+    var uid = "s" + dimension.width.feet + "x" + dimension.length.feet + "x" + dimension.height.feet + dimension.height.inches;
+    var human_readable = dimension.width.feet + "'W x " + dimension.length.feet + "'L x " + dimension.height.feet + "'";
+    if (dimension.height.inches) {
+        human_readable += dimension.height.inches + "\"H";
+    } else {
+        human_readable += "H";
+    }
+
+    $('<input/>', {
+        type: 'radio',
+        id: uid,
+        value: uid,
+        name: 'dimension'
+    }).appendTo(container);
+
+    $('<label/>', {
+        'for': uid,
+        text: human_readable
+    }).appendTo(container);
+}
+
+var barn_dimensions = [
+    {
+        width:
+            {
+                feet: 8,
+                inches: 0
+            },
+        length:
+            {
+                feet: 8,
+                inches: 0
+            },
+        height:
+            {
+                feet: 8,
+                inches: 5
+            }
+    },
+    {
+        width:
+            {
+                feet: 8,
+                inches: 0
+            },
+        length:
+            {
+                feet: 10,
+                inches: 0
+            },
+        height:
+            {
+                feet: 8,
+                inches: 5
+            }
+    },
+    {
+        width:
+            {
+                feet: 8,
+                inches: 0
+            },
+        length:
+            {
+                feet: 12,
+                inches: 0
+            },
+        height:
+            {
+                feet: 8,
+                inches: 5
+            }
+    },
+    {
+        width:
+            {
+                feet: 10,
+                inches: 0
+            },
+        length:
+            {
+                feet: 10,
+                inches: 0
+            },
+        height:
+            {
+                feet: 9,
+                inches: 5
+            }
+    },
+    {
+        width:
+            {
+                feet: 10,
+                inches: 0
+            },
+        length:
+            {
+                feet: 12,
+                inches: 0
+            },
+        height:
+            {
+                feet: 9,
+                inches: 5
+            }
+    },
+    {
+        width:
+            {
+                feet: 10,
+                inches: 0
+            },
+        length:
+            {
+                feet: 14,
+                inches: 0
+            },
+        height:
+            {
+                feet: 9,
+                inches: 5
+            }
+    },
+    {
+        width:
+            {
+                feet: 10,
+                inches: 0
+            },
+        length:
+            {
+                feet: 16,
+                inches: 0
+            },
+        height:
+            {
+                feet: 9,
+                inches: 5
+            }
+    },
+    {
+        width:
+            {
+                feet: 10,
+                inches: 0
+            },
+        length:
+            {
+                feet: 18,
+                inches: 0
+            },
+        height:
+            {
+                feet: 9,
+                inches: 5
+            }
+    },
+    {
+        width:
+            {
+                feet: 10,
+                inches: 0
+            },
+        length:
+            {
+                feet: 20,
+                inches: 0
+            },
+        height:
+            {
+                feet: 9,
+                inches: 5
+            }
+    },
+    {
+        width:
+            {
+                feet: 12,
+                inches: 0
+            },
+        length:
+            {
+                feet: 12,
+                inches: 0
+            },
+        height:
+            {
+                feet: 10,
+                inches: 0
+            }
+    },
+    {
+        width:
+            {
+                feet: 12,
+                inches: 0
+            },
+        length:
+            {
+                feet: 14,
+                inches: 0
+            },
+        height:
+            {
+                feet: 10,
+                inches: 0
+            }
+    },
+    {
+        width:
+            {
+                feet: 12,
+                inches: 0
+            },
+        length:
+            {
+                feet: 16,
+                inches: 0
+            },
+        height:
+            {
+                feet: 10,
+                inches: 0
+            }
+    },
+    {
+        width:
+            {
+                feet: 12,
+                inches: 0
+            },
+        length:
+            {
+                feet: 18,
+                inches: 0
+            },
+        height:
+            {
+                feet: 10,
+                inches: 0
+            }
+    },
+    {
+        width:
+            {
+                feet: 12,
+                inches: 0
+            },
+        length:
+            {
+                feet: 20,
+                inches: 0
+            },
+        height:
+            {
+                feet: 10,
+                inches: 0
+            }
+    },
+    {
+        width:
+            {
+                feet: 12,
+                inches: 0
+            },
+        length:
+            {
+                feet: 24,
+                inches: 0
+            },
+        height:
+            {
+                feet: 10,
+                inches: 0
+            }
+    },
+    {
+        width:
+            {
+                feet: 14,
+                inches: 0
+            },
+        length:
+            {
+                feet: 20,
+                inches: 0
+            },
+        height:
+            {
+                feet: 11,
+                inches: 5
+            }
+    },
+    {
+        width:
+            {
+                feet: 14,
+                inches: 0
+            },
+        length:
+            {
+                feet: 24,
+                inches: 0
+            },
+        height:
+            {
+                feet: 11,
+                inches: 5
+            }
+    },
+    {
+        width:
+            {
+                feet: 16,
+                inches: 0
+            },
+        length:
+            {
+                feet: 20,
+                inches: 0
+            },
+        height:
+            {
+                feet: 12,
+                inches: 3
+            }
+    },
+    {
+        width:
+            {
+                feet: 16,
+                inches: 0
+            },
+        length:
+            {
+                feet: 24,
+                inches: 0
+            },
+        height:
+            {
+                feet: 12,
+                inches: 3
+            }
+    },
+    {
+        width:
+            {
+                feet: 16,
+                inches: 0
+            },
+        length:
+            {
+                feet: 30,
+                inches: 0
+            },
+        height:
+            {
+                feet: 12,
+                inches: 3
+            }
+    },
+    {
+        width:
+            {
+                feet: 16,
+                inches: 0
+            },
+        length:
+            {
+                feet: 32,
+                inches: 0
+            },
+        height:
+            {
+                feet: 12,
+                inches: 3
+            }
+    },
+];
