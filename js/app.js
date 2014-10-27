892910a87c2ead3ae15e3197f30cb0579c3396ca
Harrison Liddiard <hliddiard@athletics.ucla.edu>
2014-10-27 11:44:30 -0700

    show/hide sections appropriately

--- js/app.js
+++ js/app.js
@@ -1,75 +1,86 @@
 (function(){
-    var app = angular.module('form', []);
+    var app = angular.module('order', []);
 
-    app.controller('FormController', function($http){
+    app.controller('FormController', function($http) {
         this.options = {};
+
+        this.section = 0;
+        this.isSelected = function(checkSection) {
+            return this.section === checkSection;
+        };
+        this.nextSection = function() {
+            this.section++;
+        };
+        this.selectSection = function(setSection) {
+            this.section = setSection;
+        };
     });
 
 
-    app.directive('dimensions', function(){
+    app.directive('dimensions', function() {
         return {
             restrict: 'A',
             templateUrl: "../views/dimensions.html"
         };
     });
 
-    app.directive('doors', function(){
+    app.directive('doors', function() {
         return {
             restrict: 'A',
             templateUrl: "../views/doors.html"
         };
     });
 
-    app.directive('windows', function(){
+    app.directive('windows', function() {
         return {
             restrict: 'A',
             templateUrl: "../views/windows.html"
         };
     });
 
-    app.directive('cupolas', function(){
+    app.directive('cupolas', function() {
         return {
             restrict: 'A',
             templateUrl: "../views/cupolas.html"
         };
     });
 
-    app.directive('shelves', function(){
+    app.directive('shelves', function() {
         return {
             restrict: 'A',
             templateUrl: "../views/shelves.html"
         };
     });
 
-    app.directive('lofts', function(){
+    app.directive('lofts', function() {
         return {
             restrict: 'A',
             templateUrl: "../views/lofts.html"
         };
     });
 
-    app.directive('roof', function(){
+    app.directive('roof', function() {
         return {
             restrict: 'A',
             templateUrl: "../views/roof.html"
         };
     });
 
-    app.directive('paint', function(){
+    app.directive('paint', function() {
         return {
             restrict: 'A',
             templateUrl: "../views/paint.html"
         };
     });
 
-    app.directive('misc', function(){
+    app.directive('misc', function() {
         return {
             restrict: 'A',
             templateUrl: "../views/misc.html"
         };
     });
 
-    app.directive('summary', function(){
+    app.directive('summary', function() {
         return {
             restrict: 'A',
             templateUrl: "../views/summary.html"
