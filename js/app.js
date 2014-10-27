4fe91dd860206360ab75590403301fb650cc21eb
Harrison Liddiard <hliddiard@athletics.ucla.edu>
2014-10-27 11:20:03 -0700

    moved each form section into its own template partial

--- js/app.js
+++ js/app.js
@@ -5,4 +5,75 @@
         this.options = {};
     });
 
+
+    app.directive('dimensions', function(){
+        return {
+            restrict: 'A',
+            templateUrl: "../views/dimensions.html"
+        };
+    });
+
+    app.directive('doors', function(){
+        return {
+            restrict: 'A',
+            templateUrl: "../views/doors.html"
+        };
+    });
+
+    app.directive('windows', function(){
+        return {
+            restrict: 'A',
+            templateUrl: "../views/windows.html"
+        };
+    });
+
+    app.directive('cupolas', function(){
+        return {
+            restrict: 'A',
+            templateUrl: "../views/cupolas.html"
+        };
+    });
+
+    app.directive('shelves', function(){
+        return {
+            restrict: 'A',
+            templateUrl: "../views/shelves.html"
+        };
+    });
+
+    app.directive('lofts', function(){
+        return {
+            restrict: 'A',
+            templateUrl: "../views/lofts.html"
+        };
+    });
+
+    app.directive('roof', function(){
+        return {
+            restrict: 'A',
+            templateUrl: "../views/roof.html"
+        };
+    });
+
+    app.directive('paint', function(){
+        return {
+            restrict: 'A',
+            templateUrl: "../views/paint.html"
+        };
+    });
+
+    app.directive('misc', function(){
+        return {
+            restrict: 'A',
+            templateUrl: "../views/misc.html"
+        };
+    });
+
+    app.directive('summary', function(){
+        return {
+            restrict: 'A',
+            templateUrl: "../views/summary.html"
+        };
+    });
+
 })();
