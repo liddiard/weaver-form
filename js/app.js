61e9ee12a0a3776a8370532bd934f43078cd24c1
Harrison Liddiard <hliddiard@athletics.ucla.edu>
2014-10-28 14:44:31 -0700

    get styles from API, style data binding

--- js/app.js
+++ js/app.js
@@ -1,7 +1,19 @@
 (function(){
     var app = angular.module('order', []);
 
+    // TODO: remove if this doesn't solve anything
+    app.config(function($httpProvider) {
+        //Enable cross domain calls
+        $httpProvider.defaults.useXDomain = true;
+
+        //Remove the header used to identify ajax call  that would prevent CORS from working
+        delete $httpProvider.defaults.headers.common['X-Requested-With'];
+    });
+
     app.controller('FormController', function($http) {
+        var form = this;
+
+        this.styles = [];
         this.options = {};
 
         this.section = 0;
@@ -14,8 +26,15 @@
         this.selectSection = function(setSection) {
             this.section = setSection;
         };
-    });
 
+        $http.get('http://peaceful-beyond-1028.herokuapp.com/styles').success(function(data){
+            form.styles = data;
+            console.log(form.styles);
+        })
+        .error(function(data, status, headers, config){
+            console.log(data);
+        });
+    });
 
     app.directive('dimensions', function() {
         return {
