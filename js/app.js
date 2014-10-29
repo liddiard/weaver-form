e75b00fa887a8a3f486a0baa78a6e78f13bb5fb8
omniaura <omniaura5@gmail.com>
2014-10-29 16:39:57 -0700

    store options and calculate base price (/treatment_price endpoint untested)

--- js/app.js
+++ js/app.js
@@ -10,11 +10,21 @@
         delete $httpProvider.defaults.headers.common['X-Requested-With'];
     });
 
-    app.controller('FormController', function($http) {
+    app.controller('FormController', function($scope, $http) {
         var form = this;
 
         this.styles = [];
-        this.options = {};
+        this.sizes = [];
+        this.features = ['Deluxe', 'Premier', 'Vinyl'];
+        this.base_prices = {};
+        this.options = {
+            style: '',
+            size: '',
+            feature: '',
+            zone: 0,
+            build_type: '',
+            finish: '',
+        };
 
         this.section = 0;
         this.isSelected = function(checkSection) {
@@ -26,14 +36,36 @@
         this.selectSection = function(setSection) {
             this.section = setSection;
         };
+        this.displaySize = function(size) {
+            if (size) return size.width + "x" + size.length;
+        }
 
         $http.jsonp('http://peaceful-beyond-1028.herokuapp.com/styles/?callback=JSON_CALLBACK').success(function(data){
             form.styles = data;
-            console.log(form.styles);
         })
         .error(function(data, status, headers, config){
             console.log(data);
         });
+
+        $scope.$watch(
+            function(scope) { return form.options.style },
+            function() {
+                $http.jsonp('http://peaceful-beyond-1028.herokuapp.com/sizes/?callback=JSON_CALLBACK', {params: {style: form.options.style}}).success(function(data){
+                    form.sizes = data;
+                });
+            }
+        );
+
+        $scope.$watch(
+            function(scope) { return form.options },
+            function() {
+                if (form.options.style.length && form.options.feature.length && form.options.zone && form.options.build_type.length && form.options.finish.length) {
+                    $http.jsonp('http://peaceful-beyond-1028.herokuapp.com/treatment_price/?callback=JSON_CALLBACK', {params: {style: form.options.style, width: form.options.size.width, length: form.options.style['length'], feature: form.options.feature, zone: form.options.zone, build_type: form.options.build_type}}).success(function(data){
+                        console.log(data);
+                    });
+                }
+            }, true // objectEquality http://stackoverflow.com/a/15721434
+        );
     });
 
     app.directive('dimensions', function() {
