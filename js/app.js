eae285a46d045be689d0697911500f67c638dfea
omniaura <omniaura5@gmail.com>
2014-11-01 15:30:22 -0700

    hide and show options based on selection

--- js/app.js
+++ js/app.js
@@ -1,21 +1,15 @@
 (function(){
     var app = angular.module('order', []);
 
-    // TODO: remove if this doesn't solve anything
-    app.config(function($httpProvider) {
-        //Enable cross domain calls
-        $httpProvider.defaults.useXDomain = true;
-
-        //Remove the header used to identify ajax call  that would prevent CORS from working
-        delete $httpProvider.defaults.headers.common['X-Requested-With'];
-    });
-
     app.controller('FormController', function($scope, $http) {
         var form = this;
 
         this.styles = [];
         this.sizes = [];
+        this.prebuilt_available = false;
+        this.finishable = false;
         this.features = ['Deluxe', 'Premier', 'Vinyl'];
+        this.noFeature = ['Porch', 'Porch 12/12 Pitch', 'Leanto']; // these styles don't have a feature selection
         this.base_prices = {};
         this.options = {
             style: '',
@@ -25,8 +19,8 @@
             build_type: '',
             finish: '',
         };
-
         this.section = 0;
+
         this.isSelected = function(checkSection) {
             return this.section === checkSection;
         };
@@ -38,6 +32,32 @@
         };
         this.displaySize = function(size) {
             if (size) return size.width + "x" + size.len;
+        }
+        this.requiresFeature = function(style) {
+            for (var i = 0; i < this.noFeature.length; i++) {
+                if (style === this.noFeature[i])
+                    return false;
+            }
+            return true;
+        }
+        this.validBaseOptions = function() {
+            if (!this.options.style.length || !this.options.size || !this.options.zone) {
+                console.log('missing style or size or zone');
+                return false; // everything has to have these options
+            }
+            if (this.prebuilt_available && !this.options.build_type.length) {
+                console.log('missing build type');
+                return false;
+            }
+            if (this.finishable && !this.options.finish.length) {
+                console.log('missing finish');
+                return false;
+            }
+            if (this.requiresFeature(this.options.style) && !this.options.feature.length) {
+                console.log('missing feature');
+                return false;
+            }
+            return true;
         }
 
         $http.jsonp('http://peaceful-beyond-1028.herokuapp.com/styles/?callback=JSON_CALLBACK').success(function(data){
@@ -59,14 +79,32 @@
         $scope.$watch(
             function(scope) { return form.options },
             function() {
-                if (form.options.style.length && form.options.feature.length && form.options.zone && form.options.build_type.length && form.options.finish.length) {
-                    console.log(form.options.size);
+                if (form.options.style.length && form.options.size && form.options.feature) {
+                    $http.jsonp('http://peaceful-beyond-1028.herokuapp.com/finishable/?callback=JSON_CALLBACK', {params: {style: form.options.style, width: form.options.size.width, len: form.options.size.len, feature: form.options.feature}}).success(function(data){
+                        form.finishable = data;
+                    });
+                }
+                if (form.validBaseOptions()) {
                     $http.jsonp('http://peaceful-beyond-1028.herokuapp.com/prices/?callback=JSON_CALLBACK', {params: {style: form.options.style, width: form.options.size.width, len: form.options.size.len, feature: form.options.feature, zone: form.options.zone, build_type: form.options.build_type}}).success(function(data){
                         console.log(data);
                     });
                 }
             }, true // objectEquality http://stackoverflow.com/a/15721434
         );
+
+        $scope.$watch(
+            function(scope) { return form.options.size },
+            function() {
+                if (form.options.style.length) {
+                    $http.jsonp('http://peaceful-beyond-1028.herokuapp.com/prebuilt_available/?callback=JSON_CALLBACK', {params: {style: form.options.style, width: form.options.size.width, len: form.options.size.len}}).success(function(data){
+                        form.prebuilt_available = data;
+                        if (!form.prebuilt_available)
+                            form.options.build_type = 'AOS';
+                    });
+                }
+            }, true // objectEquality http://stackoverflow.com/a/15721434
+        );
+
     });
 
     app.directive('dimensions', function() {
@@ -138,5 +176,4 @@
             templateUrl: "../views/summary.html"
         };
     });
-
 })();
