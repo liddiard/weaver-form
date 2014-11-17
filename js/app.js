8423e809ba455161e2db2a731c575bc4010be398
omniaura <omniaura5@gmail.com>
2014-11-16 16:18:23 -0800

    send a POST request for price calc instead of a JSONP GET

--- js/app.js
+++ js/app.js
@@ -89,7 +89,7 @@
             return;
         };
         this.submit = function() {
-            $http.jsonp('http://peaceful-beyond-1028.herokuapp.com/prices/?callback=JSON_CALLBACK', {data: {options: form.options, additions: form.additions}}).success(function(data){
+            $http.post('http://peaceful-beyond-1028.herokuapp.com/prices/', {data: {options: form.options, additions: form.additions}}).success(function(data){
                 alert(data);
             })
             .error(function(data, status, headers, config){
