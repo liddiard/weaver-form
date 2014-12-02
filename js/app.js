ffe3caf3f70bf5c38a4dc0424c1c32872c6731fe
omniaura <omniaura5@gmail.com>
2014-12-01 23:25:32 -0800

    hardcoded max range for roof pitch, store and send additional attrs for added custom fields

--- js/app.js
+++ js/app.js
@@ -16,6 +16,9 @@
         this.finishable = false; // is a finish selection availaable by default? Can be changed by api call.
         this.features = ['Deluxe', 'Premier', 'Vinyl']; // available types of features
         this.noFeature = ['Porch', 'Porch 12/12 Pitch', 'Leanto']; // these styles don't have a feature selection
+        this.hasMaximum = [ // these range-input components have a max value associated with them
+            {name: "Roof Pitch", max: 9},
+        ];
         this.base_price = 0;
         this.totals = {};
         this.options = {
@@ -91,16 +94,28 @@
                 form.additions = data;
             });
         };
-
         this.addCustomField = function(components) {
             components.push({
-                "form_type": "text",
-                "price": 0
+                price: 0,
+                options: [],
+                // use the same properties as the first element (which comes from the API) for the following
+                form_type: "text",
+                name: components[0].name,
+                pricing_type: components[0].pricing_type
             });
         };
         this.deleteCustomField = function(components, index) {
             components.splice(index, 1);
-        }
+        };
+        this.getMaximum = function(component) {
+            // if the passed component has a minimum, return it
+            var name = component.name.toLowerCase();
+            for (var i = 0; i < form.hasMaximum.length; i++) {
+                if (name === form.hasMaximum[i].name.toLowerCase())
+                    return form.hasMaximum[i].max;
+            }
+            return;
+        };
         this.submit = function() {
             alert('form submitted');
         };
