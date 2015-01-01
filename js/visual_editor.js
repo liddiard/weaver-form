aeae6d1342af782c4ffb7eb2e2c213a9b655e906
omniaura <omniaura5@gmail.com>
2015-01-01 09:53:11 -0800

    started work on visual editor: drag and drop with dummy elements

---
+++ js/visual_editor.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+    window.setTimeout(function(){
+
+        $('.draggable').draggable({
+            containment: 'document',
+            helper: 'clone',
+            appendTo: 'body',
+            start: function(){
+                $(this).css('display', 'none');
+            },
+            stop: function(){
+                $(this).css('display', 'block');
+            }
+        });
+        $('#blueprint').droppable({
+            accept: '.draggable',
+            drop: function(event, ui){
+                var blueprint = $(this);
+                ui.draggable.appendTo(blueprint).css({
+                    position: 'absolute',
+                    top: ui.offset.top - $(this).offset().top,
+                    left: ui.offset.left - $(this).offset().left
+                });
+            }
+        });
+
+    }, 500);
+});
